c449789a8213e67780fffae69584c7ffeabb5cd1
ai-fast-track <farid@itecpedia.com>
2020-10-20 11:39:46 -0400

    updated to OpenCV version

--- fastapi/masks.py
+++ fastapi/masks.py
@@ -2,16 +2,17 @@ __all__ = [
     "load_model",
     "image_from_url",
     "predict",
-    "show_prediction",
     "get_masks",
 ]
 
-from mantisshrimp.all import *
+from icevision.all import *
+
 # from PIL import Image
 import PIL, requests
 import torch
 from torchvision import transforms
 import io
+
 
 def load_model(class_map=class_map, url=None):
     if url is None:
@@ -25,6 +26,7 @@ def load_model(class_map=class_map, url=None):
         model.load_state_dict(state_dict)
         return model
 
+
 def image_from_url(url):
     res = requests.get(url, stream=True)
     img = PIL.Image.open(res.raw)
@@ -32,10 +34,9 @@ def image_from_url(url):
 
 
 def predict(
-    model, image_url, detection_threshold: float = 0.5, mask_threshold: float = 0.5
+    model, image, detection_threshold: float = 0.5, mask_threshold: float = 0.5
 ):
-    img = image_from_url(image_url)
-
+    img = np.array(image)
     tfms_ = tfms.A.Adapter([tfms.A.Normalize()])
     # Whenever you have images in memory (numpy arrays) you can use `Dataset.from_images`
     infer_ds = Dataset.from_images([img], tfms_)
@@ -47,33 +48,66 @@ def predict(
         detection_threshold=detection_threshold,
         mask_threshold=mask_threshold,
     )
-
     return samples[0]["img"], preds[0]
 
 
-def show_prediction(img, pred, bbox=False, class_map=None):
-    """Returns a PIL image"""
-    show_pred(
+# def show_prediction(img, pred, bbox=False, class_map=None):
+#     """Returns a PIL image"""
+#     show_pred(
+#         img=img,
+#         pred=pred,
+#         class_map=class_map,
+#         denormalize_fn=denormalize_imagenet,
+#         show=True,
+#         bbox=bbox,
+#     )
+
+#     # Grab image from the current matplotlib figure
+#     fig = plt.gcf()
+#     fig.canvas.draw()
+#     fig_arr = np.array(fig.canvas.renderer.buffer_rgba())
+#     img = PIL.Image.fromarray(fig_arr)
+#     return img
+
+# def get_masks(model, binary_image, class_map=None):
+#     input_image = PIL.Image.open(io.BytesIO(binary_image)).convert("RGB")
+#     img, pred = predict(model=model, image=input_image)
+#     return show_prediction(img=img, pred=pred, class_map=class_map)
+
+
+def get_masks(model, binary_image, class_map=None):
+    input_image = PIL.Image.open(io.BytesIO(binary_image)).convert("RGB")
+    img, pred = predict(model=model, image=input_image)
+
+    img = draw_pred(
         img=img,
         pred=pred,
         class_map=class_map,
         denormalize_fn=denormalize_imagenet,
-        show=True,
-        bbox=bbox,
+        display_label=True,
+        display_bbox=True,
+        display_mask=True,
     )
-
-    # Grab image from the current matplotlib figure
-    fig = plt.gcf()
-    fig.canvas.draw()
-    fig_arr = np.array(fig.canvas.renderer.buffer_rgba())
-    img = PIL.Image.fromarray(fig_arr)
-
+    img = PIL.Image.fromarray(img)
+    # print("Output Image: ", img.size, type(img))
     return img
 
-def get_masks(model, img_url, class_map=None):
-    if img_url:
-        img, pred = predict(
-            model=model,
-            image_url=img_url
-        )
-        return show_prediction(img=img, pred=pred, class_map=class_map)
+
+#################################
+
+
+# def get_masks(input_image, display_list, detection_threshold, mask_threshold):
+#     display_label = ("Label" in display_list)
+#     display_bbox = ("BBox" in display_list)
+#     display_mask = ("Mask" in display_list)
+
+#     if detection_threshold==0: detection_threshold=0.5
+#     if mask_threshold==0: mask_threshold=0.5
+
+#     img, pred = predict(model=model, image=input_image, detection_threshold=detection_threshold, mask_threshold=mask_threshold)
+#     # print(pred)
+
+#     img = draw_pred(img=img, pred=pred, class_map=class_map, denormalize_fn=denormalize_imagenet, display_label=display_label, display_bbox=display_bbox, display_mask=display_mask)
+#     img = PIL.Image.fromarray(img)
+#     # print("Output Image: ", img.size, type(img))
+#     return img
